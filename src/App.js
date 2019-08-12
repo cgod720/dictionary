ef07d796d44fe43c5089fee720825daec24b70ba
Carlos Godoy <cgod720@gmail.com>
2019-08-11 23:39:28 -0400

    tried different api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      app_id: "6edf648a",
+      app_key: "214b1cb9e2430304f013a45bfdbc5ce9",
+      wordId: "",
+      fields: "pronunciations",
+      strictMatch: "false",
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.id]: event.target.value})
+  }
+
+
+  /*fetchWordDef = (event) => {
+    event.preventDefault()
+    fetch(`https://od-api.oxforddictionaries.com/api/v2/entries/en-us/${this.state.wordId}`)
+      .then((response) => {
+        console.log(response);
+        return response.json()
+      })
+      .then((jData) => {
+        console.log(jData);
+      })
+      .catch((err) => {
+      console.log(err);
+    });
+    console.log('hello');
+  };*/
+
+  fetchWords = (event) => {
+    event.preventDefault()
+    fetch('http//localhost:5000/words')
+      .then(data => data.json())
+      .then(jData => console.log(jData))
+  }
+
+
+
+  render(){
+    return (
+      <div className="App">
+        <form onSubmit={this.fetchWords}>
+
+          <input type="submit" value="Define"/>
+        </form>
+      </div>
+    );
+  }
+
 }
 
 export default App;
